37fbe40dbe8209d627df94891187a1fe3a38def6
kirti <kirti.pandey1515@gmail.com>
2025-04-30 17:41:34 +0530

    first commit for selenium automation

---
+++ conftest.py
@@ -0,0 +1,61 @@
+import time
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name", action = "store", default = "chrome"
+    )
+
+@pytest.fixture(scope="class")
+def setup(request):
+
+    browser_Name = request.config.getoption("browser_name")
+    if browser_Name == "chrome":
+        service_obj = Service("C:\chromedriver-win64\chromedriver-win64\chromedriver.exe")
+        driver= webdriver.Chrome(service = service_obj )
+
+    elif browser_Name == "firefox" :
+        print("firefox")
+    elif browser_Name == "IE" :
+        print("IE")
+
+    driver.get("https://www.saucedemo.com/")
+    driver.maximize_window()
+    request.cls.driver = driver
+    yield
+    time.sleep(5)
+    driver.close()
+
+"""
+
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item):
+    
+       # Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+       # :param item:
+        
+
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+
+    if report.when == 'call' or report.when == "setup":
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            file_name = report.nodeid.replace("::", "_") + ".png"
+            _capture_screenshot(file_name)
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                extra.append(pytest_html.extras.html(html))
+        report.extra = extra
+
+
+def _capture_screenshot(name):
+    driver.get_screenshot_as_file(name)
+        
+"""
---
+++ test_saucedemo.py
@@ -0,0 +1,12 @@
+import time
+
+from page_objsauce.saucedemo import Saucedemo
+from utilities.basesauce import BaseSauce
+
+
+class Testsausedemo(BaseSauce):
+    def test_saucedemo(self):
+        self.Login()
+        time.sleep(2)
+        sau=Saucedemo(self.driver)
+        sau.sauce()

eb9c380e80f2ce28773d0a805c60bb6d40a4b32f
kirti <kirti.pandey1515@gmail.com>
2025-05-01 20:31:10 +0530

    sdet commit to feature_abc

--- test_saucedemo.py
+++ test_saucedemo.py
@@ -10,5 +10,9 @@ class Testsausedemo(BaseSauce):
         time.sleep(2)
         sau=Saucedemo(self.driver)
         sau.sauce()
+<<<<<<< HEAD
 #another sample file by sdet2
+=======
+#new change
+>>>>>>> 24e284b083aaa78195ad833c8a597ac8d09c2777
 #sampleline
